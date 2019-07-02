ee64a5da24ed120070abdcadd371fbb924ef6e59
ancilcrayton <ancil.crayton@ucdconnect.ie>
2019-07-02 18:41:05 -0400

    Add function to tag the documents

--- utils.py
+++ utils.py
@@ -6,13 +6,21 @@ from string import digits
 
 # Function to preprocess the text, lowercasing, removing digits and stopwords, and lemmatizing
 def text_preprocessing(raw_text):
-	# Tokenize and lowercase text
-	lower = CountVectorizer().build_tokenizer()(raw_text.lower())
-	# Remove digits
+    # Tokenize and lowercase text
+    lower = CountVectorizer().build_tokenizer()(raw_text.lower())
+    # Remove digits
     no_digits = [w for w in lower if not w.isdigit()]
-	# Remove stopwords
+    # Remove stopwords
     filtered = [w for w in no_digits if not w in stopwords.words('english')]
     # Lemmatize the text
-	lemmatizer = WordNetLemmatizer()
+    lemmatizer = WordNetLemmatizer()
     lemma = [lemmatizer.lemmatize(w) for w in filtered]
     return ' '.join(lemma)
+
+# Function to tag the documents for training
+def tag_documents(text):
+    # Tag documents with index
+    tagged_docs = [TaggedDocument(words=word_tokenize(doc), tags=[i]) for i, doc in enumerate(text)]
+    return tagged_docs
+
+
