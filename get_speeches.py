d83650ba052f5f7c7bf680eca5e092c626f0e1f3
ancilcrayton <ancil.crayton@ucdconnect.ie>
2019-07-02 12:13:57 -0400

    Fix indentation errors and import datetime as d

--- get_speeches.py
+++ get_speeches.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import datetime
+import datetime as d
 import os
 import re
 import requests
@@ -22,9 +22,9 @@ def scrape_speeches():
         # Choose the proper webpage to access
     	print('Collecting data for year {}...'.format(year))
     	if year < 2011:
-        	page = 'https://www.federalreserve.gov/newsevents/speech/{}speech.htm'.format(year)
+            page = 'https://www.federalreserve.gov/newsevents/speech/{}speech.htm'.format(year)
     	if year >= 2011:
-        	page = 'https://www.federalreserve.gov/newsevents/speech/{}-speeches.htm'.format(year)
+            page = 'https://www.federalreserve.gov/newsevents/speech/{}-speeches.htm'.format(year)
     	# Access page
     	page_response = requests.get(page)
     	page_content = BeautifulSoup(page_response.content, 'html.parser')
@@ -36,7 +36,7 @@ def scrape_speeches():
     	# Get all of the links to the articles
     	urls = []
     	for link in links:
-        	urls.append(link['href'])
+            urls.append(link['href'])
     	# Begin the process of scraping and collecting the data
     	dates = []
     	speakers = []
@@ -45,38 +45,38 @@ def scrape_speeches():
     	base_url = 'https://www.federalreserve.gov'
     	print('Scraping speeches...'.format(year))
     	for url in urls:
-        	link = base_url+url
-        	# Access page
-        	page_response = requests.get(link)
-        	page_content = BeautifulSoup(page_response.content, 'html.parser')
-        	# Collect data and append to lists
-        	# dates
-        	date = page_content.find('p', attrs={'class':'article__time'}).text
-        	dates.append(date)
-        	# speakers
-        	speaker = page_content.find('p', attrs={'class':'speaker'}).text
-        	speakers.append(speaker)
-        	# locations
-        	location = page_content.find('p', attrs={'class':'location'}).text
-        	locations.append(location)
-        	# text
-        	block = page_content.find('div', attrs={'class':'col-xs-12 col-sm-8 col-md-8'})
-        	paragraphs = block.find_all('p')
-        	text_list = []
-        	for paragraph in paragraphs:
-            	text_list.append(paragraph.text)
-        	text = ' '.join(text_list)
-        	speech_text.append(text)
-        	# Save info as a dataframe
-        	df = pd.DataFrame(data={'Date':dates, 'Speaker':speakers, 'Location':locations, 'Speech':speech_text})
+            link = base_url+url
+            # Access page
+            page_response = requests.get(link)
+            page_content = BeautifulSoup(page_response.content, 'html.parser')
+            # Collect data and append to lists
+            # dates
+            date = page_content.find('p', attrs={'class':'article__time'}).text
+            dates.append(date)
+            # speakers
+            speaker = page_content.find('p', attrs={'class':'speaker'}).text
+            speakers.append(speaker)
+            # locations
+            location = page_content.find('p', attrs={'class':'location'}).text
+            locations.append(location)
+            # speech text
+            block = page_content.find('div', attrs={'class':'col-xs-12 col-sm-8 col-md-8'})
+            paragraphs = block.find_all('p')
+            text_list = []
+            for paragraph in paragraphs:
+                text_list.append(paragraph.text)
+            text = ' '.join(text_list)
+            speech_text.append(text)
+            # Save info as a dataframe
+            df = pd.DataFrame(data={'Date':dates, 'Speaker':speakers, 'Location':locations, 'Speech':speech_text})
     	# Append data frame to the main loop
     	dfs.append(df)
     	print('{} is done!'.format(year))
-	# Combine every year to form dataset
-	data = pd.concat(dfs, ignore_index = True)
+    # Combine every year to form dataset
+    data = pd.concat(dfs, ignore_index = True)
 	
-	# Save the dataset
-	data.to_json('../data/fed_speeches.json', orient='records')
+    # Save the dataset
+    data.to_json('../data/fed_speeches.json', orient='records')
 
 # Execute function from command line
 if __name__ == '__main__':
