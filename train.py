c130dee8091c5cbe169d0b9cd817ca9b09c9c6d3
ancilcrayton <ancil.crayton@ucdconnect.ie>
2019-07-02 19:45:01 -0400

    Edit syntax of vecsize argument

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from gensim.models.doc2vec import Doc2Vec
 # Get arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', help='Number of epochs to train', default=25, type=int)
-parser.add_argument('--vec-size', help='Size of embedding vector', default=300, type=int)
+parser.add_argument('--vec_size', help='Size of embedding vector', default=300, type=int)
 parser.add_argument('--alpha', help='Learning rate', default=0.025)
 parser.add_arguement('--algorithm', help="Choose distributed memory ('DM') or Distributed Bag of Words ('DBOW')", default='DM')
 args = parser.parse_args()
@@ -30,7 +30,7 @@ print('done tagging.')
 print('Building model...')
 
 max_epochs = args.epochs
-vec_size = args.vec-size
+vec_size = args.vec_size
 alpha = args.alpha
 cpus = multiprocessing.cpu_count()
 
