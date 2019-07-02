087d24de943c6168ea9642ece7a903550059f9d7
ancilcrayton <ancil.crayton@ucdconnect.ie>
2019-07-02 18:26:04 -0400

    Add function for preprocessing text

---
+++ utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from string import digits
+
+# Function to preprocess the text, lowercasing, removing digits and stopwords, and lemmatizing
+def text_preprocessing(raw_text):
+	# Tokenize and lowercase text
+	lower = CountVectorizer().build_tokenizer()(raw_text.lower())
+	# Remove digits
+    no_digits = [w for w in lower if not w.isdigit()]
+	# Remove stopwords
+    filtered = [w for w in no_digits if not w in stopwords.words('english')]
+    # Lemmatize the text
+	lemmatizer = WordNetLemmatizer()
+    lemma = [lemmatizer.lemmatize(w) for w in filtered]
+    return ' '.join(lemma)
